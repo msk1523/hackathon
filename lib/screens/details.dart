387ca1efe9c12aa07b4ac39ad6b8b8edf606fab6
Sivakumaran M <mskmss1516@gmail.com>
2024-12-19 17:21:45 +0530

    Hackathon ver-2 done

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hackathon/screens/donate.dart';
 
 class Details extends StatefulWidget {
   final String collectionName;
@@ -26,70 +27,107 @@ class _DetailsState extends State<Details> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Icon(
-                  Icons.arrow_back,
-                  color: Colors.black,
-                )),
-            StreamBuilder<DocumentSnapshot>(
-              stream: getContentDetails(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  )),
+              StreamBuilder<DocumentSnapshot>(
+                stream: getContentDetails(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
 
-                if (!snapshot.hasData || !snapshot.data!.exists) {
-                  return Center(child: Text("No content available."));
-                }
+                  if (!snapshot.hasData || !snapshot.data!.exists) {
+                    return Center(child: Text("No content available."));
+                  }
 
-                final data = snapshot.data!.data() as Map<String, dynamic>;
+                  final data = snapshot.data!.data() as Map<String, dynamic>;
 
-                return SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            data['title'] ?? '',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            data['description'] ?? '',
-                            style: TextStyle(color: Colors.white, fontSize: 14),
-                          ),
-                          SizedBox(height: 10),
-                          if (data['imageURL'] != null)
-                            Image.network(
-                              data['imageURL'],
-                              height: 150,
-                              width: 150,
+                  return SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data['title'] ?? '',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
                             ),
-                        ],
+                            SizedBox(height: 10),
+                            Text(
+                              data['description'] ?? '',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 14),
+                            ),
+                            SizedBox(height: 10),
+                            if (data['imageURL'] != null)
+                              Image.network(
+                                data['imageURL'],
+                                height: 150,
+                                width: 150,
+                              ),
+                          ],
+                        ),
                       ),
                     ),
+                  );
+                },
+              ),
+              SizedBox(height: 30),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Donate()));
+                },
+                child: Center(
+                  child: Container(
+                    width: 150,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.monetization_on,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Donate",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
-                );
-              },
-            ),
-          ],
+                ),
+              ),
+              SizedBox(height: 40)
+            ],
+          ),
         ),
       ),
     );
