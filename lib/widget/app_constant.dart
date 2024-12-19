b932f2fd707386d1a086ec62abb6a8c111a0f477
Sivakumaran M <110098069+msk1523@users.noreply.github.com>
2024-12-19 17:39:13 +0530

    Updated app_constant.dart

--- lib/widget/app_constant.dart
+++ lib/widget/app_constant.dart
@@ -1,5 +1,5 @@
 String publishableKey =
-    'pk_test_51QHqwOL4yjDRXmvEl1p3Q2BCL8zgttRTBoMcRVwo7w51WCm7y0T6v6ILiGbL2mcHBkTGUMdvpEBZ5xn3U4vYatb700djkUAS9J';
+    'PUBLISHABLE_KEY';
 String secretKey =
-    'sk_test_51QHqwOL4yjDRXmvESTARA6OlhJzFBfWuzOmO8OcNSbOnW4N5LV64rZayq65ysjxhhoqRFW44rD3UHBFfxYPUZh6700m3bKfdDW';
-String apiKey = 'AIzaSyB8I22mTsbrsRSLW_-DX0Cxny-wZQv7XZs';
+    'CECRET_KEY';
+String apiKey = 'API_KEY';
